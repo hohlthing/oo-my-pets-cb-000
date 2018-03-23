524909d8f81e1ff5339ac8ef0be72a180a880660
michael hohl <mhohl@rocketmail.com>
2018-03-23 18:39:45 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,8 +2,9 @@ class Cat
   attr_reader :name
   attr_accessor :mood
 
-def initialize(name)
-  @name = name
-end
-  
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+
 end
